6a37264b4896f0277bba609976e3bb02fc014c44
pavelgodx <pavelgodx@gmail.com>
2023-06-13 23:51:18 +0300

    +

--- keyboards/__init__.py
+++
@@ -1,4 +0,0 @@
-from keyboards.client_inline import *
-from keyboards.admin_inline import *
-from keyboards.admin_kb import *
-from keyboards.client_kb import *
--- utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from utils.date_utils import *
-from utils.file_utils import *
-from utils.text_utils import *
